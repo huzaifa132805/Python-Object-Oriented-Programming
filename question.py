4d609ac56e18aebdd453ebd3ab10a1f973ead247
Muhammad Huzaifa <huzaifa132805@gmail.com>
2023-12-25 11:02:18 +0500

    added new file

--- question.py
+++ question.py
@@ -1,12 +1,3 @@
-# class Book:
-#     def __init__(self,author,book_name,qty):
-#         self.authour = author
-#         self.book_name = book_name
-#         self.qty = qty
-#     def details(self):
-#         return f"The author name is {self.authour}\nThe Book Name is {self.book_name}\nThe quantity is {self.qty}"
-# b = Book("Nolan","Batman",120)
-# print(b.details())
 class Mobile:
     def __init__(self,mobile_name,battery_life,camera):
         self.mobile_name=mobile_name
